60cf7abe3609fc7ab1568642e30023966dc8d1ee
Robert Nogueira <robertlucasnogueira@gmail.com>
2023-04-26 11:28:54 -0300

    reactor: linter

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ with open('README.md', 'r', encoding='UTF-8') as f:
     readme = f.read()
 
 setup(name=r'squarecloud-api',
-    version='0.2.1',
-    license='MIT License',
-    author='Robert Nogueira',
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author_email='robertlucasnogueira@gmail.com',
-    keywords='squarecloud api python',
-    description='SquareCloud API wrapper',
-    packages=['squarecloud'],
-    install_requires=['aiohttp~=3.8.3'],)
+      version='3.0.1',
+      license='MIT License',
+      author='Robert Nogueira',
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      author_email='robertlucasnogueira@gmail.com',
+      keywords='squarecloud/api/python',
+      description='SquareCloud API wrapper',
+      packages=['squarecloud'],
+      package_dir={'squarecloud': 'squarecloud'},
+      package_data={'squarecloud': ['http/*.py']},
+      install_requires=['aiohttp~=3.8.3']
+      )
