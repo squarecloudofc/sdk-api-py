eb79370abb5f27040a29f7d8fbdcb8dd6f162d51
Robert Nogueira <robertlucasnogueira@gmail.com>
2023-08-10 13:24:52 -0300

    :sparkles: feat: Added attributes to all classes that inherit from RequestError

--- squarecloud/errors.py
+++ squarecloud/errors.py
@@ -5,17 +5,33 @@ class SquareException(BaseException):
 class RequestError(SquareException):
     """raised when a request fails"""
 
+    def __init__(self, route: str, status_code: int, code: str):
+        self.route = route
+        self.status = status_code
+        self.code = code
+        message = f'route [{route}] returned {status_code}, [{code}]'
+        super().__init__(message)
+
 
 class AuthenticationFailure(RequestError):
     """raised when an API token is invalid"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class NotFoundError(RequestError):
     """raises when a request returns a 404 response"""
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 
 class BadRequestError(RequestError):
     """raises when a request returns a 400 response"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class ApplicationNotFound(SquareException):
@@ -29,16 +45,28 @@ class InvalidFile(SquareException):
 class MissingConfigFile(RequestError):
     """raised when the configuration file is missing"""
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 
 class MissingDependenciesFile(RequestError):
     """raised when the configuration file is missing"""
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 
 class TooManyRequests(RequestError):
     """raised when there are too many requests"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class FewMemory(RequestError):
     """
     raised when there is insufficient memory available to host an application.
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
