9e5cbd2653f187518de0117b936345de8440a561
Robert Nogueira <robertlucasnogueira@gmail.com>
2023-09-13 07:30:22 -0300

    :books: update example

--- examples/capture_listener.py
+++ examples/capture_listener.py
@@ -2,17 +2,16 @@ import squarecloud as square
 from squarecloud import Endpoint
 
 client = square.Client('API_KEY', debug=False)
-apps = await client.all_apps()
-app = apps[0]
-
-
-@app.capture(endpoint=Endpoint.logs())
-async def on_logs_request(before, after):
-    if after != before:
-        print(f'New logs!!! {after}')
 
 
 async def example():
+    app = await client.app('application_id')
+
+    @app.capture(endpoint=Endpoint.logs())
+    async def on_logs_request(before, after):
+        if after != before:
+            print(f'New logs!!! {after}')
+
     await app.logs()  # True
 
     await app.logs()  # False
