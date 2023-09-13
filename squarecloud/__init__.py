fa06f0692d500a9cceea8a5fcf4ab832f4cb47f4
Robert Nogueira <robertlucasnogueira@gmail.com>
2023-09-13 07:46:36 -0300

    :recycle: refactor: reformatted import

--- squarecloud/__init__.py
+++ squarecloud/__init__.py
@@ -1,6 +1,18 @@
 from __future__ import annotations
 
 from .client import Client, create_config_file
-from .errors import *
+from .errors import (
+    ApplicationNotFound,
+    AuthenticationFailure,
+    BadRequestError,
+    FewMemory,
+    InvalidFile,
+    MissingConfigFile,
+    MissingDependenciesFile,
+    NotFoundError,
+    RequestError,
+    SquareException,
+    TooManyRequests,
+)
 from .file import File
 from .http.endpoints import Endpoint
