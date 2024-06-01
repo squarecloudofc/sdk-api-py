3e1c3304f734b299e441bf7ad264a845df10ca9d
robert <99199842+Robert-Nogueira@users.noreply.github.com>
2024-06-01 05:09:55 -0300

    📜 update scripts

--- scripts/clear_test_apps.py
+++ scripts/clear_test_apps.py
@@ -12,6 +12,6 @@ client = squarecloud.Client(os.getenv('KEY'))
 @run_async_script
 async def delete_test_apps():
     for app in await client.all_apps():
-        if 'test' in app.tag:
+        if '_test' in app.tag:
             await app.delete()
             print(f'\U0001F5D1 Deleted app {app.tag}, with id {app.id}...')
